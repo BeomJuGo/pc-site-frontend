99fca3f9ed593bf6eaf8440755c6994502814b02
주예지 <165743668+ace0211@users.noreply.github.com>
2025-04-09 18:40:26 +0900

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ const Header = () => {
       <div className="max-w-7xl mx-auto flex items-center justify-between p-4">
         <Link to="/" className="text-2xl font-bold text-gray-800">GoodPricePC</Link>
 
-        {/* 검색창 */}
         <div className="flex-1 mx-6">
           <input
             type="text"
@@ -15,10 +14,14 @@ const Header = () => {
           />
         </div>
 
-        {/* 로그인 버튼 */}
-        <Link to="/login" className="bg-blue-500 hover:bg-blue-600 text-white rounded-full px-6 py-2 transition">
-          로그인
-        </Link>
+        <div className="space-x-4">
+          <Link to="/login" className="bg-blue-500 hover:bg-blue-600 text-white rounded-full px-5 py-2 transition">
+            로그인
+          </Link>
+          <Link to="/signup" className="bg-green-500 hover:bg-green-600 text-white rounded-full px-5 py-2 transition">
+            회원가입
+          </Link>
+        </div>
       </div>
     </header>
   );

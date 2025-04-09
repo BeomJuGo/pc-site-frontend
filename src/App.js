04ca892bb1c459ce936848cfcdf7c47eebdcfe73
주예지 <165743668+ace0211@users.noreply.github.com>
2025-04-09 18:59:43 +0900

    Update and rename Router.tsx to App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import Home from "./pages/Home";
 import Category from "./pages/Category";
 import Detail from "./pages/PartDetail";
 import Recommend from "./pages/Recommend";
-import Login from "./pages/Login"; // 추가
-import Signup from "./pages/Signup"; // 추가
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Header from "./components/Header";
 
 const AppRouter = () => {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/category/:category" element={<Category />} />
         <Route path="/detail/:category/:id" element={<Detail />} />
         <Route path="/ai-recommend" element={<Recommend />} />
-        <Route path="/login" element={<Login />} /> {/* 추가 */}
-        <Route path="/signup" element={<Signup />} /> {/* 추가 */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
